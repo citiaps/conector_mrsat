6e1fec0841d1cbe8ce97809bc3063d986dcf7cd6
matiescudero <matias.escudero.b@usach.cl>
2022-06-28 16:43:25 -0400

    nuevas funciones

--- mrsat_conector.py
+++ mrsat_conector.py
@@ -1,45 +1,37 @@
 import sys
 import os
 import logging
-import http.client
-import urllib.parse
-import xml.dom.minidom
+import json
 import pandas as pd
-from zeep import Client, Settings
+from zeep import Client, Settings, helpers
 from zeep.transports import Transport
 from requests import Session
 from requests.auth import HTTPBasicAuth
-from zeep import helpers
-from sqlalchemy import create_engine
-from sqlalchemy import text
-from datetime import date
+from sqlalchemy import create_engine, text
+from datetime import date, datetime
 
 
- 
- ####################################
- #### CAMBIAR REPLACE POR APPEND ####
- ####################################
-def append_new_records(df, config_data, mapstore_engine, table_name, logger):
+
+def append_new_records(df, config_data, db_engine, logger):
     """Append the records from the past 3 days to the database table with toxicologic data.
 
     Args:
         df (pandas.core.frame.DataFrame): Dataframe with the new records.
         config_data (dict): config.json parameters.
         db_engine (sqlalchemy.engine.base.Engine): Database sqlalchemy engine.
-        table_name (str): Name of the output table on the database.
-    
+        
     Raises:
         SAWarning: Did not recognize type 'geometry' of column 'geom'
     """
 
-    df.to_sql(table_name, 
-                mapstore_engine, 
-                if_exists = 'replace', 
-                schema = config_data['mapstore']['schema'], 
+    df.to_sql(config_data['sernapesca']['table'], 
+                db_engine, 
+                if_exists = 'append', 
+                schema = config_data['sernapesca']['schema'], 
                 index = False)
 
-    print("[OK] - " + table_name + " dataframe successfully copied to Mapstore database")
-    logger.debug("[OK] - " + table_name.upper() + " DF_TO_DB")
+    print("[OK] - new records successfully appended to " + config_data['sernapesca']['table'] + " table")
+    logger.debug("[OK] - APPEND_NEW_RECORDS")
 
 
 def delete_old_records(db_engine, sql_query, logger):
@@ -52,8 +44,8 @@ def delete_old_records(db_engine, sql_query, logger):
 
     with db_engine.connect().execution_options(autocommit=True) as con:
         con.execute(sql_query)
-    print("[OK] - SQL query successfully executed")
-    logger.debug("[OK] - EXECUTE_SQL_QUERY")
+    print("[OK] - Old records successfully deleted")
+    logger.debug("[OK] - DELETE_OLD_RECORDS")
 
 def open_sql_query(sql_file, logger):
     """Open the passed SQL query as a sqlalchemy text clause.
@@ -108,9 +100,42 @@ def create_db_connection(config_data, logger):
     logger.debug("[OK] - CREATE_DB_CONNECTION")
     return db_connection   
 
+##############
+def dict_to_df(response_dict):
+    df = pd.DataFrame(response_dict["Sdtsnp"]["SDTSNP.SDTSNPItem"])
+    return df
 
-### FUNCIONES PARA EXTRAER LA INFORMACIÓN NECESARIA DEL WEB SERVICE
+def reponse_to_dict(ws_response):
+    response_dict = helpers.serialize_object(ws_response)
+    return response_dict
 
+def get_ws_response(config_data, client):
+    ws_reponse = client.service.Execute(Usuario = config_data['webservice']['user'], 
+                                        Password = config_data['webservice']['passwd'],
+                                        Fechaconsulta = str(date.today()),
+                                        Numerodias = 59)
+    return ws_reponse
+
+def make_client(ws_url, session, settings):
+    client = Client(ws_url, transport = Transport(session= session), settings= settings)
+    return client
+
+def set_settings():
+    settings = Settings(strict = False)
+    return settings
+
+def set_user_and_psswd(session, config_data):
+    session.auth = (config_data['webservice']['user'], config_data['webservice']['passwd'])
+
+def generate_session():
+    session = Session()
+    return session
+
+def generate_ws_url_string(config_data):
+    ws_url = config_data['webservice']['url']
+    return ws_url
+
+################
 
 def create_logger(log_file):
     """Create a logger based on the passed log file.
@@ -177,3 +202,65 @@ def get_parameters(argv):
 
     config_filepath = argv[1]
     return config_filepath
+
+
+def main(argv):
+    start = datetime.now()
+
+    # Get parameters
+    config_filepath = get_parameters(argv)
+
+    # Get service config parameters
+    config_data = get_config(config_filepath)
+
+    # Create the log file if not exists
+    log_file = create_log_file(config_data["log_path"])
+
+    # Create the logger
+    logger = create_logger(log_file)
+
+    # Get the web service URL
+    ws_url = generate_ws_url_string(config_data)
+
+    # Generate the client's session
+    session = generate_session()
+
+    # Set the session's user and password
+    set_user_and_psswd(session, config_data)
+
+    # Set the session's settings
+    settings = set_settings()
+
+    # Make the webservice's consumer client
+    client = make_client(ws_url, session, settings)
+
+    # Get the WebService response
+    ws_response = get_ws_response(config_data, client)
+
+    # Transform the WebService response into a Python dictionary
+    response_dict = reponse_to_dict(ws_response)
+
+    # Transform python dict into Pandas DataFrame
+    df = dict_to_df(response_dict)
+
+    # Create string with the given database parameters
+    db_connection = create_db_connection(config_data, logger)
+
+    # Create sqlalchemy engine based on database parameters
+    db_engine = create_db_engine(db_connection, logger)
+
+    # Open the "delete_recent_records.sql" file
+    sql_query = open_sql_query("delete_recent_records.sql", logger)
+
+    # Delete the past 3 days records from the database table
+    delete_old_records(db_engine, sql_query, logger)
+
+    # Append the new records to the database table, extracted from the WebService
+    append_new_records(df, config_data, db_engine, logger)
+
+    end = datetime.now()
+
+    print("[OK] - New records successfully appended to " + config_data['sernapesca']['table'] + f" table. Time elapsed: {end - start}")
+
+if __name__ == "__main__":
+    main(sys.argv)
