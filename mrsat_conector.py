7d1e4126049d9b6ca38f4516eaf3185c2fc3c5cd
matiescudero <matias.escudero.b@usach.cl>
2022-06-29 18:54:06 -0400

    documentacion

--- mrsat_conector.py
+++ mrsat_conector.py
@@ -10,8 +10,6 @@ from requests.auth import HTTPBasicAuth
 from sqlalchemy import create_engine, text
 from datetime import date, datetime
 
-
-
 def append_new_records(df, config_data, db_engine, logger):
     """Append the records from the past 3 days to the database table with toxicologic data.
 
@@ -35,7 +33,7 @@ def append_new_records(df, config_data, db_engine, logger):
 
 
 def delete_old_records(db_engine, sql_query, logger):
-    """Execute the given sql query on the database, which deletes all the records from the past 3 days.  
+    """Executes the given sql query on the database, which deletes all the records from the past 3 days.  
 
     Args:
         db_engine (sqlalchemy.engine.base.Engine): Database sqlalchemy engine.
@@ -48,7 +46,7 @@ def delete_old_records(db_engine, sql_query, logger):
     logger.debug("[OK] - DELETE_OLD_RECORDS")
 
 def open_sql_query(sql_file, logger):
-    """Open the passed SQL query as a sqlalchemy text clause.
+    """Opens the passed SQL query as a sqlalchemy text clause.
 
     Args:
         sql_file (str): Name of the .sql file to execute
@@ -65,7 +63,7 @@ def open_sql_query(sql_file, logger):
 
 
 def create_db_engine(db_connection, logger):
-    """Create sqlalchemy database engine based on the database connection string.
+    """Creates a sqlalchemy database engine based on the database connection string.
 
     Args:
         db_connection (str): string with the databse connection.
@@ -80,7 +78,7 @@ def create_db_engine(db_connection, logger):
     return db_engine
 
 def create_db_connection(config_data, logger):
-    """Create the database connection string based on the config file parameters.
+    """Creates the database connection string based on the config file parameters.
 
     Args:
         config_data (dict): config.json parameters.
@@ -100,45 +98,117 @@ def create_db_connection(config_data, logger):
     logger.debug("[OK] - CREATE_DB_CONNECTION")
     return db_connection   
 
-##############
-def dict_to_df(response_dict):
+def dict_to_df(response_dict, logger):
+    """Transforms the Python dict to a pandas DataFrame.
+
+    Args:
+        response_dict (collections.OrderedDict): past 60 days toxicologic records from the mrSAT WebService.
+
+    Returns:
+        pandas.core.frame.DataFrame
+    """
     df = pd.DataFrame(response_dict["Sdtsnp"]["SDTSNP.SDTSNPItem"])
+    print("[OK] - Python dictionary successfully transformed to pandas DataFrame")
+    logger.debug("[OK] - DICT_TO_DF")
     return df
 
-def reponse_to_dict(ws_response):
+def response_to_dict(ws_response, logger):
+    """Transforms the zeep response object to a Python dictionary.
+
+    Args:
+        ws_response (zeep.objects.WS_MRSNP.ExecuteResponse): past 60 days toxicologic records from the mrSAT WebService.
+
+    Returns:
+        collections.OrderedDict
+    """
     response_dict = helpers.serialize_object(ws_response)
+    print("[OK] - Web service response successfully transformed to python dictionary")
+    logger.debug("[OK] - RESPONSE_TO_DICT")
     return response_dict
 
-def get_ws_response(config_data, client):
+def get_ws_response(config_data, client, logger):
+    """Gets the past 60 days toxicologic records from the mrSAT WebService as a zeep response object.
+
+    Args:
+        config_data (dict): config.json parameters.
+        client (zeep.client.Client): zeep Client object.
+
+    Returns:
+        zeep.objects.WS_MRSNP.ExecuteResponse
+    """
     ws_reponse = client.service.Execute(Usuario = config_data['webservice']['user'], 
                                         Password = config_data['webservice']['passwd'],
                                         Fechaconsulta = str(date.today()),
-                                        Numerodias = 59)
+                                        Numerodias = 2)
+    print("[OK] - Web Service response successfully gotten")
+    logger.debug("[OK] - GET_WS_RESPONSE")
     return ws_reponse
 
-def make_client(ws_url, session, settings):
+def make_client(ws_url, session, settings, logger):
+    """Generate the WebService client, based on the WS URL and the session and settings parameters.
+
+    Args:
+        ws_url (str): WebService URL.
+        session (requests.sessions.Session): WebService client's session.
+        settings(zeep.settings.Settings): WebService client's settings.
+
+    Returns:
+        zeep.client.Client
+    """
     client = Client(ws_url, transport = Transport(session= session), settings= settings)
+    print("[OK] - Web client successfully created")
+    logger.debug("[OK] - MAKE_CLIENT")
     return client
 
-def set_settings():
+def set_settings(logger):
+    """Sets the WebService client's session settings.
+
+    Returns:
+        zeep.settings.Settings
+    """
     settings = Settings(strict = False)
+    print("[OK] - Web client settings successfully setted")
+    logger.debug("[OK] - SET_SETTINGS")
     return settings
 
-def set_user_and_psswd(session, config_data):
-    session.auth = (config_data['webservice']['user'], config_data['webservice']['passwd'])
+def set_user_and_psswd(session, config_data, logger):
+    """Sets the session credentials, obtained from the config file.
 
-def generate_session():
+    Args:
+        session (requests.sessions.Session): Web Service client's session object
+        config_data (dict): config.json parameters.
+    """
+    session.auth = (config_data['webservice']['user'], config_data['webservice']['passwd'])
+    logger.debug("[OK] - SET_USER_AND_PSSWD")
+    print("[OK] - User credentials successfully setted")
+
+def generate_session(logger):
+    """Generates the Web Service client's session object.
+
+    Returns:
+        requests.sessions.Session
+    """
     session = Session()
+    print("[OK] - Client's session successfully generated")
+    logger.debug("[OK] - GENERATE_SESSION")
     return session
 
-def generate_ws_url_string(config_data):
+def generate_ws_url_string(config_data, logger):
+    """Stores the WebService URL as a string, obtained from the config file.
+
+    Args:
+        config_data (dict): config.json parameters.
+
+    Returns:
+        str
+    """
     ws_url = config_data['webservice']['url']
+    print("[OK] - WebService URL string successfully generated")
+    logger.debug("[OK] - GENERATE_WS_URL_STRING")
     return ws_url
 
-################
-
 def create_logger(log_file):
-    """Create a logger based on the passed log file.
+    """Creates a logger based on the passed log file.
 
     Args:
         log_file (str): Path of the log file.
@@ -155,7 +225,7 @@ def create_logger(log_file):
     return logger
 
 def create_log_file(log_path):
-    """Create the log folder if not exists. Get the log file name.
+    """Creates the log folder if not exists. Get the log file name.
 
     Args:
         log_path (str): Path of the log folder.
@@ -170,7 +240,7 @@ def create_log_file(log_path):
     return log_file 
 
 def get_config(filepath=""):
-    """Read the config.json file.
+    """Reads the config.json file.
 
     Args:
         filepath (string):  config.json file path
@@ -191,7 +261,7 @@ def get_config(filepath=""):
     return data
 
 def get_parameters(argv):
-    """Store the input parameters.
+    """Stores the input parameters.
 
     Args:
         argv (list):  input parameters
@@ -220,28 +290,28 @@ def main(argv):
     logger = create_logger(log_file)
 
     # Get the web service URL
-    ws_url = generate_ws_url_string(config_data)
+    ws_url = generate_ws_url_string(config_data, logger)
 
     # Generate the client's session
-    session = generate_session()
+    session = generate_session(logger)
 
     # Set the session's user and password
-    set_user_and_psswd(session, config_data)
+    set_user_and_psswd(session, config_data, logger)
 
     # Set the session's settings
-    settings = set_settings()
+    settings = set_settings(logger)
 
     # Make the webservice's consumer client
-    client = make_client(ws_url, session, settings)
+    client = make_client(ws_url, session, settings, logger)
 
     # Get the WebService response
-    ws_response = get_ws_response(config_data, client)
+    ws_response = get_ws_response(config_data, client, logger)
 
     # Transform the WebService response into a Python dictionary
-    response_dict = reponse_to_dict(ws_response)
+    response_dict = response_to_dict(ws_response, logger)
 
     # Transform python dict into Pandas DataFrame
-    df = dict_to_df(response_dict)
+    df = dict_to_df(response_dict, logger)
 
     # Create string with the given database parameters
     db_connection = create_db_connection(config_data, logger)
@@ -255,7 +325,7 @@ def main(argv):
     # Delete the past 3 days records from the database table
     delete_old_records(db_engine, sql_query, logger)
 
-    # Append the new records to the database table, extracted from the WebService
+    # Append the new records extracted from the WebServiceto the database table
     append_new_records(df, config_data, db_engine, logger)
 
     end = datetime.now()
