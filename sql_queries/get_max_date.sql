4de595f7d7f3a4b202caa7c6f856feb1538ffc1e
matiescudero <matias.escudero.b@usach.cl>
2022-07-05 21:33:44 -0400

    nuevas consultas

---
+++ sql_queries/get_max_date.sql
@@ -0,0 +1,11 @@
+/*
+CONTEXTO:
+- Se requiere seleccionar la última fecha registrada en la tabla existente.
+
+RESULTADOS ESPERADOS:
+- Registro único de la máxima fecha de la tabla
+
+*/
+
+SELECT MAX("FechaExtraccion") FROM {}.{};
+
