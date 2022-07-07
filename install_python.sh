d234ef48fba80811ee4810c84d5f2401c6e4e7d9
manacho <manuel.manriquez.b@usach.cl>
2022-07-07 13:39:21 -0400

    [Fix] Script de instalación de Python

--- install_python.sh
+++ install_python.sh
@@ -1,10 +1,11 @@
 Sudo apt update & sudo apt upgrade
+sudo apt isntall git
+git clone https://github.com/python/cpython.git
+cd cpython
+git checkout v3.9.2
 sudo apt install wget build-essential libreadline-gplv2-dev libncursesw5-dev \
-libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
-wget https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz
-tar xzf Python-3.9.1.tgz
-cd Python-3.9.1
+libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev libpq-dev libgdbm-compat-dev
 . /configure --enable-optimizations
-make -j 2
-sudo make alt install
+make -j 4
+sudo make install
 done
