3485cf912262710c280efd04a28476ff6a32d20f
manacho <manuel.manriquez.b@usach.cl>
2022-07-07 13:56:36 -0400

    [Fix] Script de instalación de Python - se añaden paquetes

--- install_python.sh
+++ install_python.sh
@@ -4,7 +4,7 @@ git clone https://github.com/python/cpython.git
 cd cpython
 git checkout v3.9.2
 sudo apt install wget build-essential libreadline-gplv2-dev libncursesw5-dev \
-libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev libpq-dev libgdbm-compat-dev
+libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev libpq-dev libgdbm-compat-dev unixodbc-dev 
 . /configure --enable-optimizations
 make -j 4
 sudo make install
