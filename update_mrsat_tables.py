4cb596651b369edaea30015399324fbedc5be43b
matiescudero <matias.escudero.b@usach.cl>
2022-07-14 19:33:01 -0400

    prueba con autocommit true

--- update_mrsat_tables.py
+++ update_mrsat_tables.py
@@ -301,7 +301,7 @@ def generate_connection(db_engine, logger):
         sqlalchemy.engine.base.Connection
     """
     try:
-        db_con = db_engine.connect().execution_options(autocommit=False)
+        db_con = db_engine.connect().execution_options(autocommit=True)
         print("[OK] - Successfully connected to the database engine")
         logger.debug("[OK] - GENERATE_CONNECTION")
         return db_con
