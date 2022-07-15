ca8804e1fd4b7a7645008ab6dbecafe129971da4
matiescudero <matias.escudero.b@usach.cl>
2022-07-14 21:22:58 -0400

    test begin-end

--- sql_queries/delete_for_check.sql
+++ sql_queries/delete_for_check.sql
@@ -6,6 +6,7 @@ RESULTADOS ESPERADOS:
 - Eliminar todos los registros con máximo 4 días de antigüedad EN BASE A LA MÁXIMA FECHA DE EXTRACCIÓN. 
 
 */
-
+BEGIN
 DELETE FROM {0}.{1}
 WHERE "FechaExtraccion" >= DATEADD(DAY, -3, (SELECT MAX("FechaExtraccion") FROM {0}.{1}));
+END
