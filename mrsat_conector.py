09484e83e0fa5b515287cd9bcc12e53bd7aa8b08
matiescudero <matias.escudero.b@usach.cl>
2022-07-15 12:40:20 -0400

    request not critical

--- mrsat_conector.py
+++ mrsat_conector.py
@@ -422,7 +422,7 @@ def create_logger(log_file):
                     format='%(asctime)s %(message)s',
                     filemode='a')
 
-    logger = logging.getLogger('requests').setLevel(logging.CRITICAL)
+    logger = logging.getLogger('requests')
     logger.getLogger("zeep").setLevel(logging.CRITICAL)
     logger.setLevel(logging.DEBUG)
     return logger
