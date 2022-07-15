a63d9b6fb59196e8d9ab885b2c97c64973cb52f5
matiescudero <matias.escudero.b@usach.cl>
2022-07-15 12:28:13 -0400

    zeep to critical

--- mrsat_conector.py
+++ mrsat_conector.py
@@ -423,7 +423,7 @@ def create_logger(log_file):
                     filemode='a')
 
     logger = logging.getLogger('requests')
-    logging.getLogger("zeep").propagate = False
+    logger = logging.getLogger("zeep").setLevel(logging.CRITICAL)
     logger.setLevel(logging.DEBUG)
     return logger
 
